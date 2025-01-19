84a5d948bd277ca9b128cb9a5b22f4f2623aa999
John Rizzo <johnrizzo1@gmail.com>
2025-01-18 20:55:47 -0500

    Added CV and cleaned up the site a bit.

--- devenv.nix
+++ devenv.nix
@@ -1,21 +1,21 @@
 { pkgs, lib, config, inputs, ... }:
-let 
-  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+let
+    pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
 in
 {
-  languages.nix.enable = true;
-  languages.javascript.enable = true;
-  languages.javascript.npm.enable = true;
-  languages.typescript.enable = true;
-  languages.go.enable = true;
+    languages.nix.enable = true;
+    languages.javascript.enable = true;
+    languages.javascript.npm.enable = true;
+    languages.typescript.enable = true;
+    languages.go.enable = true;
 
-  packages = [ pkgs.git pkgs-unstable.hugo ];
-  enterShell = ''
-    git --version
-  '';
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-  # See full reference at https://devenv.sh/reference/options/
+    packages = [ pkgs.git pkgs-unstable.hugo ];
+    enterShell = ''
+        git --version
+    '';
+    enterTest = ''
+        echo "Running tests"
+        git --version | grep --color=auto "${pkgs.git.version}"
+    '';
+    # See full reference at https://devenv.sh/reference/options/
 }
